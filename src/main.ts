611afcd78e1ac086df5c56f7f0b107b315835c96
kheanglov <lovsokheang@gmail.com>
2022-04-11 22:53:12 +0700

    update build

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 import Vue3ColorPicker from "vue3-colorpicker"
 import "vue3-colorpicker/style.css"
 import './assets/style.css'
 const intervalMS = 60 * 60 * 1000
-const updateSW = registerSW({
-  onNeedRefresh(r) {
-    r && setInterval(() => {
-        r.update()
-    }, intervalMS)
-  },
-  onOfflineReady() {},
-})
+// const updateSW = registerSW({
+//   onNeedRefresh(r) {
+//     r && setInterval(() => {
+//         r.update()
+//     }, intervalMS)
+//   },
+//   onOfflineReady() {},
+// })
 
 const app = createApp(App)
-app.use(updateSW)
+// app.use(updateSW)
 app.use(Vue3ColorPicker)
 app.mount('#app')
