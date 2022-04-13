fd13475c34e37af3376e7c88a1ddf8cb5ebf55a1
kheanglov <lovsokheang@gmail.com>
2022-04-13 21:55:59 +0700

    remove unused

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,22 @@
 import { createApp } from 'vue'
 // @ts-ignore
 import { registerSW } from 'virtual:pwa-register'
-import Vue3ColorPicker from 'vue3-colorpicker'
 // @ts-ignore
 import QrReader from 'vue3-qr-reader'
 // @ts-ignore
 import LitepieDatepicker from 'litepie-datepicker'
-// @ts-ignore
-import Toastify from 'toastify-js'
-import App from './App.vue'  
+import Vue3ColorPicker from 'vue3-colorpicker'
+import App from './App.vue'
 import 'vue3-colorpicker/style.css'
 import './assets/style.css'
 
 const app = createApp(App)
 
-if ('serviceWorker' in navigator) {    
+if ('serviceWorker' in navigator) {
+    const intervalMS = 60 * 60 * 1000
     const updateSW = registerSW({
-        onNeedRefresh() {
-            Toastify({
-                text: `<h4 style='display: inline'>An update is available!</h4>
-                    <br><br>
-                    <a class='do-sw-update'>Click to update and reload</a>  `,
-                escapeMarkup: false,
-                gravity: "bottom",
-                onClick() {
-                    updateSW(true);
-                }
-            }).showToast();
+        onNeedRefresh(r: any) {
+            r && setInterval(() => r.update(), intervalMS)
         },
     })
 
